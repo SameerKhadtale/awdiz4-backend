8fd1dddf32f253d5393d832cccc1c948e3ab38a9
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-11-04 22:50:04 +0530

    updates

--- Controllers/Auth.Controllers.js
+++ Controllers/Auth.Controllers.js
@@ -1,34 +1,49 @@
-import UserModal from "../Modals/User.modals";
-import bcrypt from 'bcrypt';
-
-export const Login = (req, res) => {
-    res.send("Hi from login Hiiii")
+export const Login =(req,res)=>{
+    res.send("Hi from login hii")
 }
+export const Register =(req,res)=>{
+    try{
+        const { name, email, passowrd} = req.body;
 
-export const Register = async (req, res) => {
-    try {
-        //console.log(req.body,"req.body")
-        const { name, email, passowrd, number } = req.body;
-        //console.log(number, typeof number)
-        //console.log(req.body,"add data")
-        if (!name || !email || !password || !number) return res.status(401).json({ success: false, message: "All fields are mandatory" })
-
-
-        const hashedPassword = bcrypt.hash(password, 10);
-        console.log(hashedPassword, "hashedPassword")
-        // const user = new UserModal({
-        //     name: name,
-        //     email,
-        //     password,
-        //     number
-        // })
-
-        await user.save();
         return res.status(200).json({ success: true, message: "Registration Successful.." })
-    } catch (error) {
+    }catch(error){
         return res.status(500).json({ success: false, message: error })
     }
 }
-export const getCurrentUser = (req, res) => {
-    res.send("HIII")
+export const getCurrentUser =(req,res)=>{
+    res.send("HII")
 }
+
+// import UserModal from "../Modals/User.modals";
+// import bcrypt from 'bcrypt';
+
+// export const Login = (req, res) => {
+//     res.send("Hi from login Hiiii")
+// }
+
+// export const Register = async (req, res) => {
+//     try {
+//         //console.log(req.body,"req.body")
+//
+//         //console.log(number, typeof number)
+//         //console.log(req.body,"add data")
+//         if (!name || !email || !password || !number) return res.status(401).json({ success: false, message: "All fields are mandatory" })
+
+
+//         const hashedPassword = bcrypt.hash(password, 10);
+//         console.log(hashedPassword, "hashedPassword")
+//         // const user = new UserModal({
+//         //     name: name,
+//         //     email,
+//         //     password,
+//         //     number
+//         // })
+//         await user.save();
+//         
+//     } catch (error) {
+//
+//     }
+// }
+// export const getCurrentUser = (req, res) => {
+//     res.send("HIII")
+// }
