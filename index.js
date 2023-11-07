a9f9ae0668c58acd437514b3add28d29c507f13e
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2023-11-07 18:19:34 +0530

    update

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-// console.log("Hi from index.js backend")
+console.log("Hi from index.js backend")
+console.log("Hi from index.js backend")
 
 
-import express from 'express';
-import router from './Router/index.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import cors from 'cors';
+// import express from 'express';
+// import router from './Router/index.js';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv';
+// import morgan from 'morgan';
+// import cors from 'cors';
 
-const app = express();
-dotenv.config();
-app.use(morgan('dev'))
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// dotenv.config();
+// app.use(morgan('dev'))
+// app.use(cors());
+// app.use(express.json());
 
-app.use((req,res, next) => {
-    console.log("hi from middleware use")
-    // res.send("hi from middleware use")
-    next();
-})
-
-app.use("/api/v1", router)
-
-mongoose.connect(process.env.MONGOURL).then(()=>console.log("Database Connected.."))
-// // import { Hello } from './Controllers/GlobalControllers.js'
-
-// const app = express()
-
-// app.get("/",function (req,res){
-// res.send("Hello Everyone..")
+// app.use((req,res, next) => {
+//     console.log("hi from middleware use")
+//     // res.send("hi from middleware use")
+//     next();
 // })
 
+// app.use("/api/v1", router)
 
-// app.get("/hello", Hello);    
+// mongoose.connect(process.env.MONGOURL).then(()=>console.log("Database Connected.."))
+// // // import { Hello } from './Controllers/GlobalControllers.js'
 
-// app.listen(8000,()=>console.log("App is running on port 8000."))
+// // const app = express()
+
+// // app.get("/",function (req,res){
+// // res.send("Hello Everyone..")
+// // })
+
+
+// // app.get("/hello", Hello);    
+
+// // app.listen(8000,()=>console.log("App is running on port 8000."))
