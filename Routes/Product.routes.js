16af7c279073dd3e03b4b44d18bd95cf644a006e
SameerKhadtale <138295791+SameerKhadtale@users.noreply.github.com>
2024-01-11 18:37:51 +0530

    updates

--- Routes/Product.routes.js
+++ Routes/Product.routes.js
@@ -5,7 +5,7 @@ import { checkUserId } from "../Middlewares/AllMiddlewares.js";
 const router = Router();
 //api
 router.post('/filter-products', filterProducts)
-router.post('/add-product', checkUserId, addProduct)
+router.post('/add-product', addProduct)
 router.get('/get-all-product', getAllProducts)
 router.get('/get-single-product', getSingleProduct)
 router.post('/your-products', yourProducts)
